2b968ed417cf415d2371099481dbf887371ae41f
abdulhammed24 <abdlhamd3@gmail.com>
2022-06-12 13:39:56 -0700

    1st commit

---
+++ src/components/Articles.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import SkeletonArticle from "../skeletons/SkeletonArticle";
+
+const url = "https://jsonplaceholder.typicode.com/posts";
+
+const Articles = () => {
+  const [articles, setArticles] = useState(null);
+
+  const fetchArticle = async () => {
+    try {
+      const response = await axios(url);
+      const data = await response.data;
+      //   console.log(data);
+      setArticles(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchArticle();
+    }, 2000);
+  }, []);
+
+  return (
+    <div className="articles">
+      <h2>All Articles</h2>
+
+      {articles &&
+        articles.map((article) => (
+          <div key={article.id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        ))}
+
+      {!articles &&
+        [1, 2, 3, 4, 5].map((n) => <SkeletonArticle key={n} theme="light" />)}
+      {/* [1, 2, 3, 4, 5].map((n) => <SkeletonArticle key={n} theme="dark" />)} */}
+    </div>
+  );
+};
+
+export default Articles;
---
+++ src/components/User.js
@@ -0,0 +1,44 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SkeletonProfile from "../skeletons/SkeletonProfile";
+
+const url = "https://jsonplaceholder.typicode.com/users/1";
+
+const User = () => {
+  const [owner, setOwner] = useState(null);
+  const fetchUser = async () => {
+    try {
+      const response = await axios(url);
+      const data = await response.data;
+      //   console.log(data);
+      setOwner(data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetchUser();
+    }, 2000);
+  }, []);
+
+  return (
+    <div>
+      <h1>user</h1>
+
+      {owner && (
+        <div className="profile">
+          <h2>{owner.name}</h2>
+          <h3>{owner.username}</h3>
+          <p>{owner.email}</p>
+          <a href={owner.website}>{owner.website}</a>
+        </div>
+      )}
+
+      {!owner && <SkeletonProfile />}
+    </div>
+  );
+};
+
+export default User;
